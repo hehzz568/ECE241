7dd5e024bc83afdd4477d8314dac34fd6ef35fa7
alan h. <69955478+hehzz568@users.noreply.github.com>
2025-10-01 14:23:49 -0400

    Add files via upload

---
+++ part1.v
@@ -0,0 +1,7 @@
+// Simple module that connects the SW switches to the LEDR lights
+module part1 (SW, LEDR);
+    input [9:0] SW;     // 10 inputs
+    output [9:0] LEDR;  // 10 red leds
+
+    assign LEDR = SW;   // send every signal to the led
+endmodule
